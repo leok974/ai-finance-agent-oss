31244bbcb20b670d41bb3c2278a6951615f91426
leok974 <leoklemet.pa@gmail.com>
2025-09-09 19:06:59 -0400

    chore(migrations): merge heads after feedback timestamp + standardization

---
+++ apps/backend/alembic/versions/c4a739e0f055_merge_heads_after_feedback_timestamp_.py
@@ -0,0 +1,28 @@
+"""merge heads after feedback timestamp + standardization
+
+Revision ID: c4a739e0f055
+Revises: 20250909_feedback_created_at_not_null, b4068fc306b1
+Create Date: 2025-09-09 19:06:53.331963
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'c4a739e0f055'
+down_revision: Union[str, Sequence[str], None] = ('20250909_feedback_created_at_not_null', 'b4068fc306b1')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
